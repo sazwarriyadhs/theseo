eba5b595d69acbc46139fb458e911e8c554ebf65
Digi media <ptdigimedia@gmail.com>
2025-07-05 07:53:16 +0000

    Salin dan tempel kode AdSense di antara tag  <script async src="https://

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_PUBLISHER_ID}`}
             crossOrigin="anonymous"
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
           />
         )}
         {GA_MEASUREMENT_ID && (
